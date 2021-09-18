0a58e2a8e7d9ebfedc14d61bc529006779616523
Jaime Trevino <trevinojaime42@gmail.com>
2021-09-18 17:45:33 -0400

    errors with new package

--- src/pages/ExquisiteCorpses.js
+++ src/pages/ExquisiteCorpses.js
@@ -1,13 +1,18 @@
 import { Layout } from "../layout/Layout";
 import styled from "styled-components";
 
+//import makeSentence from "../tools/RandomWord";
+
+let x = "";
+const clickHandler = () =>{
+ }
 const ExquisiteCorpses = () => {
     return(
         <div class = "header">
             Exquisite Corpses
+            <button onClick={clickHandler}> Generate Corpse </button>
         </div>
     );
 }
-const styles = {
-}
+
 export default ExquisiteCorpses;

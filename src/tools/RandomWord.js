be8d7a946b342cb6f554fccb2df71d8a44d3d4d6
Jaime Trevino <trevinojaime42@gmail.com>
2021-09-18 17:54:40 -0400

    change default branch

--- src/tools/RandomWord.js
+++ src/tools/RandomWord.js
@@ -1,12 +1,12 @@
 //import {getAdjectives, getAdverbs, getNouns, getVerbs} from 'random-part-of-speech'
 
-a = getAdjectives(1);
-b = getNouns(1);
-c = getAdverbs(1);
-d = getVerbs(1);
-e = getAdjectives(1);
-f = getNouns(1);
+// a = getAdjectives(1);
+// b = getNouns(1);
+// c = getAdverbs(1);
+// d = getVerbs(1);
+// e = getAdjectives(1);
+// f = getNouns(1);
 
-export function makeSentence(){
-    return("The "+ a + " " + b + " " + c + " " + d + " the " + e + " "+ f +".");
-}
+// export function makeSentence(){
+//     return("The "+ a + " " + b + " " + c + " " + d + " the " + e + " "+ f +".");
+// }

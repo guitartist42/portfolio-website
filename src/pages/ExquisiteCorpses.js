ff18682cae887a70bb86c8472084b62ffd17217a
Jaime Trevino <trevinojaime42@gmail.com>
2021-09-19 20:27:21 -0400

    worked with useState hook

--- src/pages/ExquisiteCorpses.js
+++ src/pages/ExquisiteCorpses.js
@@ -1,20 +1,22 @@
+import React, {useState} from 'react';
 import { Layout } from "../layout/Layout";
 import styled from "styled-components";
 
 //import makeSentence from "../tools/RandomWord";
 //experiencing difficulties finding a package to randomly generate parts of speech, have tried 2 each with their own outdated bugs
-let x = "";
-const clickHandler = () =>{
-    x = "hello!";
- }
-const ExquisiteCorpses = () => {
+
+const ExquisiteCorpses = (props) => {
+    const [corpse, setCorpse] = useState(props.corpse);
+    const clickHandler = () =>{
+        setCorpse('hello!');
+    };
     return(
         <>
         <div class = "header">
             Exquisite Corpses
         </div>
         <button onClick={clickHandler}> Generate Corpse </button>
-        <div>Result is: {x}</div>
+        <h2>Result is: {corpse} </h2>
         </>
     );
 }

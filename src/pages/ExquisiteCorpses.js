f98ec53b8d48071345b20912d8abc5089d9a81bd
Jaime Trevino <trevinojaime42@gmail.com>
2021-09-19 20:07:03 -0400

    edits

--- src/pages/ExquisiteCorpses.js
+++ src/pages/ExquisiteCorpses.js
@@ -2,16 +2,20 @@ import { Layout } from "../layout/Layout";
 import styled from "styled-components";
 
 //import makeSentence from "../tools/RandomWord";
-
+//experiencing difficulties finding a package to randomly generate parts of speech, have tried 2 each with their own outdated bugs
 let x = "";
 const clickHandler = () =>{
+    x = "hello!";
  }
 const ExquisiteCorpses = () => {
     return(
+        <>
         <div class = "header">
             Exquisite Corpses
-            <button onClick={clickHandler}> Generate Corpse </button>
         </div>
+        <button onClick={clickHandler}> Generate Corpse </button>
+        <div>Result is: {x}</div>
+        </>
     );
 }
 

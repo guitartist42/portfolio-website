334f4ab65f332202f880fbea1b40bddf59637611
Jaime Trevino <trevinojaime42@gmail.com>
2021-09-20 23:50:51 -0400

    puppeteer addition

--- src/tools/RandomWord.js
+++ src/tools/RandomWord.js
@@ -1,12 +1,30 @@
-//import {getAdjectives, getAdverbs, getNouns, getVerbs} from 'random-part-of-speech'
+const puppeteer = require('puppeteer');
 
-// a = getAdjectives(1);
-// b = getNouns(1);
-// c = getAdverbs(1);
-// d = getVerbs(1);
-// e = getAdjectives(1);
-// f = getNouns(1);
+{/* <>
+<div src = "" id = "adjectiveOne" />
+<div src = "" id = "nounOne" />
+<div src = "" id = "adverb" />
+<div src = "" id = "verb" />
+<div src = "" id = "adjectiveTwo" />
+<div src = "" id = "nounTwo" />
+</> */}
+ 
+async function adjective(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
 
-// export function makeSentence(){
-//     return("The "+ a + " " + b + " " + c + " " + d + " the " + e + " "+ f +".");
-// }
+    const [el] = await page.$x('//*[@id="result"]/li/div/span');
+    const src = await el.getProperty('src');
+
+    console.log({el});
+
+    browser.close();
+}
+adjective('https://randomwordgenerator.com/adjective.php');
+
+// console.log("about to fectch first noun");
+// console.log("about to fetch adverb");
+// console.log("about to fetch verb");
+// console.log("about to fetch second adjective");
+// console.log("about to fetch second noun");
